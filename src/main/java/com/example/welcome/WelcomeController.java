fe58cacf96804b22176b90404ecbd0f65b7597b1
sudhakar <venkatasudhakar33@gmail.com>
2026-03-27 16:52:26 +0530

    welcome api

---
+++ src/main/java/com/example/welcome/WelcomeController.java
@@ -0,0 +1,12 @@
+package com.example.welcome;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome";
+    }
+}
